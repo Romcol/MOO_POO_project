09ee2ee24aefdb35147da4fdf18208de7a7c7c37
Romain Colombat <Romain Colombat>
2015-11-12 17:57:04 +0100

    Comments design patterns

---
+++ POO_MOO/dev/Impl/TileFlyweight.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace dev
+{
+    public class TileFlyweight
+    {
+        public Dictionary<String, Tile> mapTile
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public Tile getTile()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
