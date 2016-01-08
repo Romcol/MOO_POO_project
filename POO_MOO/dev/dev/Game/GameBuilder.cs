c12d93df60ee47293e00a6a905818a42152649da
Guillaume Perrudin <guillaume.perrudin@insa-rennes.fr>
2016-01-08 09:27:26 +0100

    modifs builder

--- POO_MOO/dev/dev/Game/GameBuilder.cs
+++ POO_MOO/dev/dev/Game/GameBuilder.cs
@@ -1,15 +1,21 @@
 ﻿using API;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 
 namespace dev
 {
     public class GameBuilder
     {
+
+		private const string FILE_SAVE = "save.dat";
+
 		public Game game = new Game();
-       public GameBuilder()
+		public GameBuilder()
         {
             this.game = new Game();
         }
@@ -39,6 +45,21 @@ namespace dev
 			return game;
 		}
 
+		public void save(string file)
+		{
+			IFormatter formatter = new BinaryFormatter();
+			Stream stream = new FileStream(file, FileMode.Create, FileAccess.Write, FileShare.None);
+			formatter.Serialize(stream, this.game);
+			stream.Close();
+		}
+		public GameAPI load(string file)
+		{
+			IFormatter formatter = new BinaryFormatter();
+			Stream stream = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.Read);
+			Game ret = (Game)formatter.Deserialize(stream);
+			stream.Close();
 
+			return ret;
+		}
 	}
 }
